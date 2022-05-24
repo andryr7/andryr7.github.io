4adb47b26b66c3c764e7a55b9c4750dc924aaf65
andryr7 <andryratsimba@gmail.com>
2022-05-24 11:51:31 +0200

    reinit css et passage php

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Andry Ratsimba</title>
+	    <link rel="stylesheet" href="css/normalize.css">
+	    <link rel="stylesheet" href="css/main.css">
+    </head>
+<body>
+    
+<!-- Menu à cacher en mobile-->
+<nav id="barrenav">
+	<ul>
+		<li><a href="#presentation">Présentation</a></li>
+		<li><a href="#competences">Compétences</a></li>
+		<li><a href="#experience">Expériences</a></li>
+		<li><a href="#formation">Formation</a></li>
+		<li><a href="#interets">Intérêts</a></li>
+	</ul>
+</nav>
+
+<!-- Décoration et carte-->
+<header>
+
+<div id="fondcolore">
+</div>
+
+<div id="carte">
+    <img id="maphoto" src="img/photo.jpg" alt="Photo de Andry Ratsimba">
+    <div id="coordonnees">
+        <p>
+            Andry Ratsimba 
+            <br>
+            Développeur web en formation 
+            <br>
+            <a href="mailto:andryratsimba@gmail.com">andryratsimba@gmail.com</a>
+            <br>
+            <a href="https://wa.me/33630696242">+33 6 30 69 62 42</a>
+            <br>
+            <a href="https://www.linkedin.com/in/andryratsimba/">LinkedIn</a>
+            <br>
+            <a href="doc/CV.pdf">Mon CV</a>
+        </p>
+    </div>
+</div>
+
+</header>
+
+<!--Présentation-->
+<section id="presentation">
+Responsable dans un petit supermarché du centre-ville de Toulouse, je suis actuellement en formation de développement web au sein de l'école O'Clock. Passionné depuis l'enfance par l'informatique, j'ai pu découvrir en détail le métier de développeur lors de deux stages en tant qu'assistant chargé de projets webmarketing, et j'ai été captivé par les aspects techniques de cette fonction. Créatif, rigoureux et passionné, je suis convaincu de pouvoir réussir dans cette voie.
+</section>
+
+
+<!--Compétences-->
+<section id="competences">
+    <h2>Compétences</h2>
+
+    <h3>Compétences-clés</h3>
+
+<ul>
+    <li>Autonomie et capacité d'autoformation</li>
+    <li>Analyse et attention aux détails</li>
+    <li>Travail en équipe et management</li>
+</ul>
+
+<h3>Compétences informatiques</h3>
+
+<ul>
+    <li><progress value="0.5"></progress>HTML, CSS</li>
+    <li><progress value="0.7"></progress>Microsoft Office</li>
+    <li><progress value="0.8"></progress>Edition audio, photo et vidéo</li>
+    <li><progress value="0.3"></progress>SEO/SEA (Google Ads, Analytics)</li>
+    <li><progress value="0.4"></progress>CMS: Prestashop, Magento, Wordpress</li>
+    <li><progress value="0.9"></progress>Réseaux Sociaux</li>
+</ul>
+
+<h3>Langues</h3>
+
+<ul>
+    <li>Anglais: courant (lu, écrit, parlé) : TOEIC 965, voyages en pays anglophones, DU Anglais des affaires</li>
+    <li>Français: participation au <a href="https://www.projet-voltaire.fr/">projet Voltaire</a></li>
+    <li>Allemand: niveau BAC</li>
+</ul>
+
+</section>
+
+<!--Expériences-->
+<section id="experiences">
+    
+    <h2>Expériences
+	</h2>
+
+    <h3>Employé principal responsable</h3>
+
+    <h3>Employé libre-service</h3>
+
+    <h3>Stagiaire assistant chargé de projets marketing</h3>
+
+    <h3>Stagiaire chargé de développement webmarketing</h3>
+
+	<h3>Stagiaire assistant au contrôle de gestion</h3>
+
+    <h3>Assistant chargé d'études et enquêteur</h3>
+
+</section>
+
+<!--Formation-->
+<section id="formation">
+
+<h2>Formation</h2>
+
+<ul>
+    <li>M1 Marketing-Vente Parcours Marketing, Communication, Etudes (2017)</li>
+    <li>Licence en Sciences de Gestion parcours Marketing-Vente</li>
+    <li>DUT Gestion des Entreprises et des Administrations - option finances et comptabilité</li>
+    <li>BAC ES option anglais renforcé</li>
+</ul>
+
+</section>
+
+<!--Intérêts-->
+<section id="interets">
+
+<h2>Intérêts</h2>
+
+<ul>
+	<li>Musique</li>
+	<li>Sport</li>
+	<li>Voyage</li>
+</ul>
+
+</section>
+
+</body>
+</html>
